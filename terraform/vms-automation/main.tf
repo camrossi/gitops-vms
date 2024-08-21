789262d0785d9d3cc45338fc3abda247fc6ab752
Camillo Rossi <camrossi@cisco.com>
2024-08-20 23:18:18 -0400

    tf

--- terraform/vms-automation/main.tf
+++ terraform/vms-automation/main.tf
@@ -2,7 +2,5 @@ resource "aci_tenant" "map" {
   for_each = {
     for k, v in var.tenants : k => v
   }
-  description                   = each.value.description
   name                          = each.key
-  name_alias                    = each.value.alias
 }
