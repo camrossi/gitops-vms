b79261270a729aadb208d629f65bb0ea0e02501a
Camillo Rossi <camrossi@cisco.com>
2024-08-21 00:59:15 -0400

    terraform

--- terraform/vms-automation/main.tf
+++ terraform/vms-automation/main.tf
@@ -3,27 +3,27 @@ data "aci_tenant" "tenant" {
 }
 
 data "aci_application_profile" "app" {
-  tenant_dn  = aci_tenant.tenant.id
+  tenant_dn  = data.aci_tenant.tenant.id
   name       = var.app
 }
 
 data "aci_application_epg" "vlans" {
   for_each = toset(var.vlans)
-  application_profile_dn  = aci_application_profile.app.id
+  application_profile_dn  = data.aci_application_profile.app.id
   name = each.key
 }
 
 resource "aci_epg_to_domain" "phys_domain" {
-  depends_on         = [aci_application_epg.vlans]
-  for_each           = aci_application_epg.vlans
-  application_epg_dn    = aci_application_epg.each.vlans.id
-  tdn                   = aci_fc_domain.foofc_domain.id
+  depends_on         = [data.aci_application_epg.vlans]
+  for_each           = data.aci_application_epg.vlans
+  application_epg_dn    = data.aci_application_epg.each.vlans.id
+  tdn                   = var.phys_domain
 }
 
 resource "aci_bulk_epg_to_static_path" "static_paths" {
-  depends_on         = [aci_application_epg.vlans]
-  for_each           = aci_application_epg.vlans
-  application_epg_dn = aci_application_epg.vlans[each.key].id
+  depends_on         = [data.aci_application_epg.vlans]
+  for_each           = data.aci_application_epg.vlans
+  application_epg_dn = data.aci_application_epg.vlans[each.key].id
   dynamic "static_path" {
     for_each = var.ports
     content {
